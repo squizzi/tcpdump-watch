7d21cf772fa212cac1bb1f74d7f3428836595cb2
Kyle Squizzato <ksquizz@gmail.com>
2013-10-24 17:27:40 -0400

    Modify redirects to redirect errors to /dev/null

--- tcpdump-nettrace.sh
+++ tcpdump-nettrace.sh
@@ -5,9 +5,8 @@ wait=3
 tcpdump="tcpdump -s0 -i any host 10.13.211.232 -W 1 -C 1024M -w $output"
 
 $tcpdump &
-pid=$!
 
-tshark -l -i any -s0 -f 'dst net 10.13.211.232' -R '(nfs.nfsstat4 == 0)' > /tmp/tshark.tmp 2>&1 &
+tshark -l -i any -s0 -f 'dst net 10.13.211.232' -R '(nfs.nfsstat4 == 0)' > /tmp/tshark.tmp 2> /dev/null &
 tail -fn 1 /tmp/tshark.tmp |
 while read line
 do
